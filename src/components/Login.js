1c660fb9a0d7091491b6a52c17a332c05321c4b5
Pranav Mudholkar <pranavmudholkar455@gmail.com>
2025-08-27 23:40:55 +0530

    Basic components created and setup. Added logo and background image for login page.

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/cb72daa5-bd8d-408b-b949-1eaef000c377/web/IN-en-20250825-TRIFECTA-perspective_a3209894-0b01-4ddb-b57e-f32165e20a3f_small.jpg"
+          alt="background-img"
+        />
+      </div>
+      <form></form>
+    </div>
+  );
+};
+
+export default Login;
